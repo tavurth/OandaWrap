77fa3af76743d7434114a95d49823ba191e88c71
Will Whitty <tavurth@gmail.com>
2014-12-13 19:08:33 +0100

    Small fixes to setup example

--- Examples/exampleSetup.php
+++ Examples/exampleSetup.php
@@ -44,22 +44,26 @@ if (defined('TAVURTH_OANDAWRAP_EXAMPLE_SETUP') == FALSE) {
 	
 	//Style our body
 	echo '<style>
-			body { font-size: 18; color:blue; }
+			body { font-size: 18; color:#222222; }
 		</style>';
 	
 	//Our header
 	echo '<h2>OandaWrap quotes test:</h2><br>';
 	
 	//Save the requested pairs as an array
-	$pairs = array('EUR_USD', 'EUR_AUD');
+	$pairs = array('EUR_USD', 'EUR_AUD', 'EUR_JPY', 'EUR_CAD');
 	
 	//Loop through the array
-	foreach ($pairs as $pair)
+	foreach ($pairs as $pair) {
+		$quote = OandaWrap::price($pair);
 		//Print our output dialog
-		echo 'Price of ' . $pair . ' is ' . OandaWrap::price($pair) . '<br>';
+		echo 'Price of ' . $pair . ' is bid:' .$quote->bid . ', offer:' . $quote->ask . ' <br>';
+	}
 	
 	//End the html
 	echo '</body>';
 	echo '</html>';
 	
 }
+
+?>
