4c89704b5bdad87869ddcf935f8f8a853029d674
Will Whitty <tavurth@gmail.com>
2015-02-08 12:19:59 +0100

    Bolded text in example

--- Examples/exampleTrade.php
+++ Examples/exampleTrade.php
@@ -41,16 +41,16 @@ if (defined('TAVURTH_OANDAWRAP_EXAMPLE_TRADE') == FALSE) {
 		exit(1);
 	} 
 	
-	echo 'Buy with a market order and included stopLoss:<br>';
+	echo '<h3><b>Buy with a market order and included stopLoss:<br></h3></b>';
 	OandaWrap::format(OandaWrap::buy_market(10, 'EUR_USD', array('stopLoss' => 1.0243)));
 	
-	echo 'Set buy limit order with included takeProfit:<br>';
+	echo '<h3><b>Set buy limit order with included takeProfit:<br></h3></b>';
 	OandaWrap::format(OandaWrap::buy_limit(10, 'EUR_USD', 1.0243, OandaWrap::expiry_day(10), array('takeProfit' => 1.032)));
 	
-	echo 'Set market if touched buy order with included trailingStop of 10 pips:<br>';
+	echo '<h3><b>Set market if touched buy order with included trailingStop of 10 pips:<br></h3></b>';
 	OandaWrap::format(OandaWrap::buy_limit(10, 'EUR_USD', 1.0243, OandaWrap::expiry_hour(), array('trailingStop' => 10)));
 	
-	echo 'Buy at market, limiting size so that 2% of account is risked over 20 pips, the set stop 20 pips from current price:<br>';
+	echo '<h3><b>Buy at market, limiting size so that 2% of account is risked over 20 pips, the set stop 20 pips from current price:<br></h3></b>';
 	OandaWrap::format(OandaWrap::buy_bullish('EUR_USD', 2, 20));
 	
 }
